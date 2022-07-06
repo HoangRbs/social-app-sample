5579405b274e143674138450b561cd6b3f2b84ed
hoangrbs <hoangrbs@gmail.com>
2022-07-06 08:44:39 +0700

    sefwef

--- client/src/components/currentChatBox/CurrentChatBox.jsx
+++ client/src/components/currentChatBox/CurrentChatBox.jsx
@@ -78,7 +78,7 @@ export default function  CurrentChatBox({
         } else {
             setIsOnline(false);
         }
-    }, [onlineUsersId]);
+    }, [onlineUsersId, currentChat]);
 
 
     useEffect(() => {
